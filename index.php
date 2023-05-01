a0bcc673f97f3f0ad883d2acf51576a1248ed035
= <=>
2023-05-01 21:41:02 +0300

    Created class Person

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+
+class Person
+{
+    public $name;
+    public $age;
+    public $gender;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param mixed $age
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param mixed $gender
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+    }
+
+
+}
+
+$person = new Person();
+
+$person->setName('John');
+$person->setAge('23');
+$person->setGender('Male');
+
+echo $person->getName();
+echo $person->getAge();
+echo $person->getGender();
